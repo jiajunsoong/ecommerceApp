987fa3829428a964c12934db065db44ad89bb40b
Soong Jia Jun <jiajun.soong@agileperspectives.com>
2023-07-11 17:20:21 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ProductListingComponent,
+    ProductComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
